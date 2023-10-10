1cb600f2da2ede2d0ca1446c32cfc8cfd3b00755
seshadriv <seshadriv@CHN-LPT1399.cns-inc.com>
2023-10-10 15:55:25 +0530

    COMMIT TEST SUITE CREATION

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>df0dbf02-ecdb-4bd0-840e-26549d3d0884</testSuiteGuid>
+</TestSuiteEntity>
